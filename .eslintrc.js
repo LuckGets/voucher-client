750253ef0f8b8f1170ce4235c24f6a300ef7194f
LuckGets <kasdies12@gmail.com>
2024-11-23 16:52:22 +0700

    first_set-up_ESLint&Prettier

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  root: true,
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: "module",
+  },
+  env: {
+    node: true,
+    es6: true,
+  },
+  extends: [
+    "next/core-web-vitals",
+    "next/typescript",
+    "eslint:recommended",
+    "prettier",
+    "plugin:react/recommended",
+    "plugin:prettier/recommended",
+    "plugin:react-hooks/recommended",
+  ],
+  plugins: ["prettier", "@typescript-eslint", "react", "react-hooks"],
+  rules: {
+    // JS rules
+    "prefer-const": "warn",
+    "react/prop-types": "off",
+    "react/react-in-jsx-scope": "off",
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "warn",
+    "prettier/prettier": "warn",
+  },
+};
